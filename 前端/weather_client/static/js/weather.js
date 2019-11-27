378a7e42bcf773115895108ba7da09d65f978169
Bruce.D <wayne_s@126.com>
2019-11-27 13:14:48 +0800

    added new functions of weatherinfo and surrounding tour info

--- 前端/weather_client/static/js/weather.js
+++ 前端/weather_client/static/js/weather.js
@@ -1,5 +1,5 @@
 
-    function temp24hShow(){
+    function temp24hShow(params){
         option = {
             dataZoom: [{
                 id: 'dataZoomX',
@@ -19,10 +19,11 @@
 
             xAxis: {
                 type: 'category',
-                data: ['01:00', '02:00', '03:00', '04:00', '05:00', '06:00',
-                    '07:00', '08:00', '09:00', '10:00', '11:00', '12:00',
-                    '13:00', '14:00', '15:00', '16:00', '17:00', '18:00',
-                    '19:00', '20:00', '21:00', '22:00', '23:00', '24:00'],
+                // data: ['01:00', '02:00', '03:00', '04:00', '05:00', '06:00',
+                //     '07:00', '08:00', '09:00', '10:00', '11:00', '12:00',
+                //     '13:00', '14:00', '15:00', '16:00', '17:00', '18:00',
+                //     '19:00', '20:00', '21:00', '22:00', '23:00', '24:00'],
+                data: params.time,
                 splitLine: { show: false },
                 axisLine: {
                     lineStyle: {
@@ -64,7 +65,8 @@
 
 
             series: [{
-                data: [23, 32, 19, 34, 29, 33, 23, 32, 19, 34, 29, 33, 23, 32, 19, 34, 29, 33, 23, 32, 19, 34, 29, 33],
+                // data: [23, 32, 19, 34, 29, 33, 23, 32, 19, 34, 29, 33, 23, 32, 19, 34, 29, 33, 23, 32, 19, 34, 29, 33],
+                data: params.temp,
                 type: 'line',
                 itemStyle: {
                     normal: {
@@ -91,9 +93,9 @@
         var echartTempLine = echarts.init(document.getElementById("weather24-1"));
         echartTempLine.setOption(option);
     }
-    temp24hShow();
+    // temp24hShow();
 
-    function wind24hShow(){
+    function wind24hShow(params){
         option = {
             dataZoom: [{
                 id: 'dataZoomX',
@@ -113,10 +115,11 @@
 
             xAxis: {
                 type: 'category',
-                data: ['01:00', '02:00', '03:00', '04:00', '05:00', '06:00',
-                    '07:00', '08:00', '09:00', '10:00', '11:00', '12:00',
-                    '13:00', '14:00', '15:00', '16:00', '17:00', '18:00',
-                    '19:00', '20:00', '21:00', '22:00', '23:00', '24:00'],
+                // data: ['01:00', '02:00', '03:00', '04:00', '05:00', '06:00',
+                //     '07:00', '08:00', '09:00', '10:00', '11:00', '12:00',
+                //     '13:00', '14:00', '15:00', '16:00', '17:00', '18:00',
+                //     '19:00', '20:00', '21:00', '22:00', '23:00', '24:00'],
+                data: params.time,
                 splitLine: { show: false },
                 axisLine: {
                     lineStyle: {
@@ -157,7 +160,8 @@
             },
 
             series: [{
-                data: [2, 2, 1, 5, 5, 6, 1, 2, 2, 5, 3, 6, 1, 5, 4, 5, 4, 5, 3, 1, 6, 6, 2, 1],
+                // data: [2, 2, 1, 5, 5, 6, 1, 2, 2, 5, 3, 6, 1, 5, 4, 5, 4, 5, 3, 1, 6, 6, 2, 1],
+                data: params.wind,
                 type: 'line',
                 itemStyle: {
                     normal: {
@@ -182,6 +186,58 @@
         var echartWindLine = echarts.init(document.getElementById("weather24-2"));
         echartWindLine.setOption(option);
     }
+    // 渲染24小时温度
+    function write_weather24_1() {
+        var $location = localStorage.getItem("location");
+        $.ajax({
+            type: "get",
+            url: "http://127.0.0.1:8000/api/v1/weather/" + $location + "?temp24=1",
+            beforeSend: function (request) {
+                result = {
+                    'code': 200,
+                    data: {
+                        'time': ['01:00', '02:00', '03:00', '04:00', '05:00', '06:00',
+                            '07:00', '08:00', '09:00', '10:00', '11:00', '12:00',
+                                '13:00', '14:00', '15:00', '16:00', '17:00', '18:00',
+                            '19:00', '20:00', '21:00', '22:00', '23:00', '24:00'],
+                        'temperature': [23, 32, 19, 34, 29, 33, 23, 32, 19, 34, 29, 33, 23, 32, 19, 34, 29, 33, 23, 32, 19, 34, 29, 33]
+                    }
+                }
+                if (200 == result.code) {
+                    temp24hShow({ time: result.data.time, temp: result.data.temperature })
+                } else {
+                    alert(result.error)
+                }
+            }
+        })
+    }
+    // 渲染24小时风力
+    function write_weather24_2() {
+        var $location = localStorage.getItem("location");
+        $.ajax({
+            type: "get",
+            url: "http://127.0.0.1:8000/api/v1/weather/" + $location + "?wind_scale24=1",
+            beforeSend: function (request) {
+                result = {
+                    'code': 200,
+                    data: {
+                        'time': ['01:00', '02:00', '03:00', '04:00', '05:00', '06:00',
+                            '07:00', '08:00', '09:00', '10:00', '11:00', '12:00',
+                            '13:00', '14:00', '15:00', '16:00', '17:00', '18:00',
+                            '19:00', '20:00', '21:00', '22:00', '23:00', '24:00'],
+                        'wind': [2, 3, 1, 3, 2, 3, 2, 2, 1, 4, 2, 3, 3, 3, 1, 4, 2, 3, 2, 3, 1, 4, 2, 3]
+                    }
+                }
+                if (200 == result.code) {
+                    console.log('test')
+                    wind24hShow({ time: result.data.time, wind: result.data.wind })
+                } else {
+                    alert(result.error)
+                }
+            }
+        })
+    }    
+
     var tempLine = document.getElementById('weather24-1');
     var windLine = document.getElementById('weather24-2');
     var elem = document.querySelectorAll('.tempOrWind>li>a');
@@ -190,14 +246,29 @@
         elem[1].className = 'aUnselected';
         tempLine.style.display = 'block';
         windLine.style.display = 'none';
-        temp24hShow();
+        // 1.查找本地存储是否有定位信息
+        if (localStorage.getItem("location")) {
+            // 2.如果有就直接发请求获取
+            write_weather24_1();
+        } else {
+            // 3.如果没有,则程序获取用户定位信息,之后再发请求获取
+            getLocation({ myfunc: write_weather24_1 })
+        }
     }
+
     elem[1].onclick = function(){
         elem[1].className = 'aSelected';
         elem[0].className = 'aUnselected';
         tempLine.style.display = 'none';
         windLine.style.display = 'block';
-        wind24hShow();
+        // 1.查找本地存储是否有定位信息
+        if (localStorage.getItem("location")) {
+            // 2.如果有就直接发请求获取
+            write_weather24_2();
+        } else {
+            // 3.如果没有,则程序获取用户定位信息,之后再发请求获取
+            getLocation({ myfunc: write_weather24_2 })
+        }
     }
 
     
